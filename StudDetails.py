a08e82a06fb3390d117c13ec7058bbe542fcbb3e
cYuanQi <87699309+cYuanQi@users.noreply.github.com>
2023-09-17 22:50:23 +0800

    Update StudDetails.py

--- StudDetails.py
+++ StudDetails.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from pymysql import connections
 import os
+import boto3
+from config import *
 
 app = Flask(__name__)
 # app.secret_key = 'your_secret_key'  # Replace with a secret key for flash messages
