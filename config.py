174f66688d4824e4a2c7de42ecb9ff4b8ea1936d
JanetSoh <101232214+JanetSoh@users.noreply.github.com>
2023-09-18 22:16:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 customhost = "database-cloud.cp0ylxbeyow9.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273!"
-customdb = "database-cloud"
+customdb = "databaseitp"
 custombucket = "janetsohjiaer-bucket"
 customregion = "us-east-1"
 
