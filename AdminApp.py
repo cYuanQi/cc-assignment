cd60fb75b5f6126804a9d54c325b37d8b3940418
JanetSoh <101232214+JanetSoh@users.noreply.github.com>
2023-09-19 23:08:22 +0800

    Add files via upload

--- AdminApp.py
+++ AdminApp.py
@@ -24,12 +24,12 @@ table = 'admin'
 def home():
     return render_template('admin.html')
 
-@app.route("/AddAdmin")
+@app.route("/AddAdmin", methods=['GET', 'POST'])
 def addAdmin():
     return render_template('AddAdmin.html')
 
 
-@app.route("/companylistadm")
+@app.route("/companylistadm", methods=['GET', 'POST'])
 def company_list():
     cursor = db_conn.cursor()
 
@@ -39,11 +39,31 @@ def company_list():
     return render_template('company_list_adm.html', rows=row)
 
 
-@app.route("/assignsupervisor")
+@app.route("/assignsupervisor", methods=['GET', 'POST'])
 def assign_supervisor():
     return render_template('assign-supervisor.html')
 
+@app.route("/assignsupervisorProcess", methods=['POST'])
+def assign_supervisorProcess():
+    stud_name = request.form['stud_name']
+    stud_id = request.form['stud_id']
+    sup_name = request.form['sup_name']
+    sup_id = request.form['sup_id']
 
+    
+    insert_sql = "INSERT INTO admin VALUES (%s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    cursor.execute(insert_sql, (stud_name, stud_id, sup_name, sup_id))
+    db_conn.commit()
+    cursor.close()
+
+    cursor = db_conn.cursor()
+    cursor.execute('SELECT * FROM admin')
+    rows = cursor.fetchall()
+    cursor.close()
+
+    return render_template('assign_supervisor_Output.html')
 
 
 if __name__ == '__main__':
