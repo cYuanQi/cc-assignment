9d0a930ef7b2e8659ea6c72da3e188f0a61160d1
KoaBX <143489354+KoaBX@users.noreply.github.com>
2023-09-21 01:05:07 +0800

    Add files via upload

--- LecApp.py
+++ LecApp.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
 from pymysql import connections
 import os
-import boto3
-from config import *
 
 app = Flask(__name__)
 
@@ -22,54 +20,43 @@ table = 'lecturer'
 def home():
     return render_template('lecturer.html')
 
-@app.route("/evaluate_report", methods=['GET', 'POST'])
+@app.route("/EvaluateReport", methods=['GET', 'POST'])
 def evaluate_report():
     if request.method == 'POST':
         # Assuming you have a form for report evaluation
         report_id = request.form['report_id']
+        report_name = request.form['report_name']
+        
         # Handle the report evaluation process here (e.g., download student report in PDF)
-
-        # For example, you can store the evaluation result in the database
-        conn = connect_to_db()
-        if conn:
-            try:
-                cursor = conn.cursor()
-                # Execute SQL statements to insert evaluation data into the database
-                # Replace with your actual SQL query
-                cursor.execute("INSERT INTO evaluation (report_id, evaluation_result) VALUES (?, ?)", (report_id, "A"))
-                conn.commit()
-                cursor.close()
-                conn.close()
-                return "Report evaluated and data inserted into the database."
-            except mariadb.Error as e:
-                print(f"Error inserting evaluation data: {e}")
-                return "An error occurred while processing the evaluation."
+        # You need to specify the logic for downloading the report in PDF format
+        # For demonstration purposes, we'll simulate it here:
+        report_pdf_path = '/Downloads/'report_name  # Replace with the actual path
+        
+        # Check if the file exists
+        if os.path.exists(report_pdf_path):
+            # Simulate sending the PDF for download
+            return send_file(report_pdf_path, as_attachment=True)
 
     return render_template('EvaluateReport.html')
 
-@app.route("/grade_report", methods=['GET', 'POST'])
+@app.route("/Grade", methods=['GET', 'POST'])
 def grade_report():
     if request.method == 'POST':
-        # Assuming you have a form for grading student reports
-        report_id = request.form['report_id']
-        student_score = request.form['student_score']
-        # Handle the grading process here
+        # Assuming you have multiple reports to grade
+        report_id = request.form['report_id']  # Replace with the corresponding report_id
+        student_score = request.form['student_score']  # Replace with the corresponding student_score
 
-        # For example, you can store the grading result in the database
-        conn = connect_to_db()
-        if conn:
-            try:
-                cursor = conn.cursor()
-                # Execute SQL statements to insert grading data into the database
-                # Replace with your actual SQL query
-                cursor.execute("INSERT INTO grades (report_id, student_score) VALUES (?, ?)", (report_id, student_score))
-                conn.commit()
-                cursor.close()
-                conn.close()
-                return "Report graded and data inserted into the database."
-            except mariadb.Error as e:
-                print(f"Error inserting grading data: {e}")
-                return "An error occurred while processing the grading."
+        # Now, you can insert the report_id and student_score into the database
+        conn = db_conn.cursor()
+        try:
+            # Execute SQL statements to insert grading data into the database
+            # Replace with your actual SQL query
+            conn.execute("INSERT INTO grades (report_id, student_score) VALUES (?, ?)", (report_id, student_score))
+            db_conn.commit()
+            return "Report graded and data inserted into the database."
+        except Exception as e:
+            print(f"Error inserting grading data: {e}")
+            return "An error occurred while processing the grading."
 
     return render_template('Grade.html')
 
