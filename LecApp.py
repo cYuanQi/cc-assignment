9d5a93e4d049e05926978718509b62c6c70441dd
KoaBX <143489354+KoaBX@users.noreply.github.com>
2023-09-22 02:14:53 +0800

    Add files via upload

--- LecApp.py
+++ LecApp.py
@@ -125,11 +125,68 @@ def gradereport():
     return render_template('Grade.html', reports=reports)
 
 
-@app.route("/nologin", methods=['GET', 'POST'])
+
+@app.route("/nologin")
 def nologin():
     return render_template('no_login.html')
+    
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
-@app.route("/lecturer")
+@app.route("/joblistings")
+def joblistings():
+    return render_template('job-listings.html')
+
+@app.route("/jobsingle")
+def jobsingle():
+    return render_template('job-single.html')
+
+@app.route("/services")
+def services():
+    return render_template('services.html')
+
+@app.route("/servicesingle")
+def servicesingle():
+    return render_template('service-single.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route("/blogsingle")
+def blogsingle():
+    return render_template('blog-single.html')
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route("/portfoliosingle")
+def portfoliosingle():
+    return render_template('portfolio-single.html')
+
+@app.route("/testimonials")
+def testimonials():
+    return render_template('testimonials.html')
+
+@app.route("/faq")
+def faq():
+    return render_template('faq.html')
+
+@app.route("/gallery")
+def gallery():
+    return render_template('gallery.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/lecture")
 def lecturer():
     return render_template('lecture.html')
 
@@ -145,9 +202,25 @@ def evaluatereport():
 def grade():
     return render_template('Grade.html')
 
-@app.route("/jobsingle")
-def jobsingle():
-    return render_template('job-single.html')
+@app.route("/company")
+def company():
+    return render_template('company.html')
+
+@app.route("/postjob")
+def postjob():
+    return render_template('post-job.html')
+
+@app.route("/studentapplyjobs")
+def studentapplyjobs():
+    return render_template('StudentApplyJobs.html')
+
+@app.route("/companylistadm")
+def companylistadm():
+    return render_template('company_list_adm.html')
+
+@app.route("/assignsupervisor")
+def assignsupervisor():
+    return render_template('assign-supervisor.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
