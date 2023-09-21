01c9d57903e455ee4c3dbbb9a840eaec3bcf009d
JDLAU1213 <143489450+JDLAU1213@users.noreply.github.com>
2023-09-21 14:04:31 +0800

    Update StudDetails.py

--- StudDetails.py
+++ StudDetails.py
@@ -104,7 +104,7 @@ def submit_student():
 
 
 # Route to display the inserted student data
-@app.route("/view_student/<user_email>", methods=['GET'])
+@app.route("/view_student/<user_email>", methods=['GET'], endpoint='display_student_data')
 def display_student_data(user_email):
     cursor = db_conn.cursor()
     select_sql = "SELECT * FROM student_detail WHERE student_email = %s"
@@ -113,6 +113,7 @@ def display_student_data(user_email):
     cursor.close()
 
     return render_template('display_student_data.html', student_data=student_data)
+
 
 
 
