42fb83db745d7e8b00ae8deffe27655492ad386f
HewWK <143489299+HewWK@users.noreply.github.com>
2023-09-22 22:09:40 +0800

    Update Company.py

--- Company.py
+++ Company.py
@@ -19,6 +19,7 @@ db_conn = connections.Connection(
     db=customdb
 )
 
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('company.html')
@@ -29,8 +30,7 @@ def company():
 
 @app.route("/postjob", methods=['GET','POST'])
 def postjob():
-    if request.method == 'POST':
-        try:
+ 
             # Get data from the form
             email = request.form['email']
             job_title = request.form['job_title']
@@ -63,40 +63,29 @@ def postjob():
                 cursor.close()
                 return "Please select a logo"
 
-            try:
+         
                 # Upload featured image and logo to S3
-                if featured_image.filename != "":
+            if featured_image.filename != "":
                     s3 = boto3.client('s3')
                     featured_image_name_in_s3 = "featured_" + featured_image.filename
                     s3.upload_fileobj(featured_image, custombucket, featured_image_name_in_s3)
                     featured_image_url = f"https://{custombucket}.s3.amazonaws.com/{featured_image_name_in_s3}"
 
-                if logo.filename != "":
+            if logo.filename != "":
                     s3 = boto3.client('s3')
                     logo_image_name_in_s3 = "logo_" + logo.filename
                     s3.upload_fileobj(logo, custombucket, logo_image_name_in_s3)
                     logo_url = f"https://{custombucket}.s3.amazonaws.com/{logo_image_name_in_s3}"
 
                 # After successfully storing in S3, store job details in the MySQL database
-                cursor.execute(insert_sql, (
+            cursor.execute(insert_sql, (
                     email, job_title, job_location, job_region, job_type, job_description,
                     company_name, company_tagline, company_description, company_website,
                     facebook_username, twitter_username, linkedin_username, featured_image_url, logo_url
-                ))
-                db_conn.commit()
-            except Exception as e:
-                cursor.close()
-                return str(e)
-            finally:
-                cursor.close()
-
-            return redirect(url_for('success'))  # Redirect to a success page or appropriate URL
-
-        except Exception as e:
-            return str(e)
-
+            ))
+            db_conn.commit()
     # If it's not a POST request, render the form
-    return render_template('post-job.html')
+            return render_template('post-job.html')
 
 
 # Define a route for the success page
