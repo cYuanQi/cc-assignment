8ae9debff4dfa93213aa2da2251c0639083593a6
HewWK <143489299+HewWK@users.noreply.github.com>
2023-09-22 15:00:45 +0800

    Update Company.py

--- Company.py
+++ Company.py
@@ -1,5 +1,5 @@
 # Import additional modules
-from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, flash
 from pymysql import connections
 import os
 import boto3
@@ -18,16 +18,10 @@ db_conn = connections.Connection(
     password=custompass,
     db=customdb
 )
-output = {}
-table = 'job_ta'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('post-job.html')
-
-@app.route("/postjob", methods=['POST'])
-def postjob():
-    try:
+    if request.method == 'POST':
         # Get data from the form
         email = request.form['email']
         job_title = request.form['job_title']
@@ -47,167 +41,62 @@ def postjob():
         featured_image = request.files['featured_image']
         logo = request.files['logo']
 
-        cursor = db_conn.cursor()
+        # Check if required fields are not empty
+        if not email or not job_title or not company_name:
+            flash('Please fill out all required fields.', 'error')
+        else:
+            try:
+                cursor = db_conn.cursor()
 
-        # Insert job data into the job table
-        insert_sql = "INSERT INTO job_table (email, job_title, job_location, job_region, job_type, job_description,company_name, company_tagline, company_description, company_website,facebook_username, twitter_username, linkedin_username, featured_image_url, logo_url) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                # Insert job data into the job table
+                insert_sql = """
+                INSERT INTO job_table (
+                    email, job_title, job_location, job_region, job_type, job_description,
+                    company_name, company_tagline, company_description, company_website,
+                    facebook_username, twitter_username, linkedin_username, featured_image_url, logo_url
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """
 
+                if featured_image and featured_image.filename != "":
+                    # Upload featured image to S3
+                    s3 = boto3.client('s3')
+                    featured_image_name_in_s3 = "featured_" + featured_image.filename
+                    s3.upload_fileobj(featured_image, custombucket, featured_image_name_in_s3)
+                    featured_image_url = f"https://{custombucket}.s3.amazonaws.com/{featured_image_name_in_s3}"
+                else:
+                    featured_image_url = None
 
-        if featured_image.filename == "":
-            cursor.close()
-            return "Please select a featured image"
+                if logo and logo.filename != "":
+                    # Upload logo to S3
+                    s3 = boto3.client('s3')
+                    logo_image_name_in_s3 = "logo_" + logo.filename
+                    s3.upload_fileobj(logo, custombucket, logo_image_name_in_s3)
+                    logo_url = f"https://{custombucket}.s3.amazonaws.com/{logo_image_name_in_s3}"
+                else:
+                    logo_url = None
 
-        if logo.filename == "":
-            cursor.close()
-            return "Please select a logo"
+                # After successfully storing in S3, store job details in the MySQL database
+                cursor.execute(insert_sql, (
+                    email, job_title, job_location, job_region, job_type, job_description,
+                    company_name, company_tagline, company_description, company_website,
+                    facebook_username, twitter_username, linkedin_username, featured_image_url, logo_url
+                ))
+                db_conn.commit()
+                cursor.close()
 
-        try:
-            # Upload featured image and logo to S3
-            if featured_image.filename != "":
-                s3 = boto3.client('s3')
-                featured_image_name_in_s3 = "featured_" + featured_image.filename
-                s3.upload_fileobj(featured_image, custombucket, featured_image_name_in_s3)
-                featured_image_url = f"https://{custombucket}.s3.amazonaws.com/{featured_image_name_in_s3}"
+                flash('Job data submitted successfully!', 'success')
+                return redirect(url_for('success'))  # Redirect to a success page or appropriate URL
+            except Exception as e:
+                flash(str(e), 'error')
+                return redirect(url_for('home'))
 
-            if logo.filename != "":
-                s3 = boto3.client('s3')
-                logo_image_name_in_s3 = "logo_" + logo.filename
-                s3.upload_fileobj(logo, custombucket, logo_image_name_in_s3)
-                logo_url = f"https://{custombucket}.s3.amazonaws.com/{logo_image_name_in_s3}"
+    return render_template('post-job.html')
 
-            # After successfully storing in S3, store job details in the MySQL database
-            cursor.execute(insert_sql, (
-                email, job_title, job_location, job_region, job_type, job_description,
-                company_name, company_tagline, company_description, company_website,
-                facebook_username, twitter_username, linkedin_username, featured_image_url, logo_url
-            ))
-            db_conn.commit()
-        except Exception as e:
-            cursor.close()
-            return str(e)
-        finally:
-            cursor.close()
-
-        return redirect(url_for('success'))  # Redirect to a success page or appropriate URL
-
-    except Exception as e:
-        return str(e)
-
-# Define a route for the success page
 @app.route("/success")
 def success():
     return "Job data submitted successfully!"
 
-    
-@app.route("/nologin", methods=['GET', 'POST'])
-def nologin():
-    return render_template('no_login.html')
-
-    
-@app.route("/about", methods=['GET', 'POST'])
-def about():
-    return render_template('about.html')
-
-@app.route("/joblistings", methods=['GET', 'POST'])
-def joblistings():
-    return render_template('job-listings.html')
-
-@app.route("/jobsingle", methods=['GET', 'POST'])
-def jobsingle():
-    return render_template('job-single.html')
-
-@app.route("/services", methods=['GET', 'POST'])
-def services():
-    return render_template('services.html')
-
-@app.route("/servicesingle", methods=['GET', 'POST'])
-def servicesingle():
-    return render_template('service-single.html')
-
-@app.route("/blog", methods=['GET', 'POST'])
-def blog():
-    return render_template('blog.html')
-
-@app.route("/blogsingle", methods=['GET', 'POST'])
-def blogsingle():
-    return render_template('blog-single.html')
-
-@app.route("/portfolio", methods=['GET', 'POST'])
-def portfolio():
-    return render_template('portfolio.html')
-
-@app.route("/portfoliosingle", methods=['GET', 'POST'])
-def portfoliosingle():
-    return render_template('portfolio-single.html')
-
-@app.route("/testimonials", methods=['GET', 'POST'])
-def testimonials():
-    return render_template('testimonials.html')
-
-@app.route("/faq", methods=['GET', 'POST'])
-def faq():
-    return render_template('faq.html')
-
-@app.route("/gallery", methods=['GET', 'POST'])
-def gallery():
-    return render_template('gallery.html')
-
-@app.route("/contact", methods=['GET', 'POST'])
-def contact():
-    return render_template('contact.html')
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
-
-@app.route("/lecture", methods=['GET', 'POST'])
-def lecture():
-    return render_template('lecture.html')
-
-@app.route("/lecturerdetails", methods=['GET', 'POST'])
-def lecturerdetails():
-    return render_template('lecturer-details.html')
-
-@app.route("/evaluatereport", methods=['GET', 'POST'])
-def evaluatereport():
-    return render_template('EvaluateReport.html')
-
-@app.route("/grade", methods=['GET', 'POST'])
-def grade():
-    return render_template('Grade.html')
-
-@app.route("/company", methods=['GET', 'POST'])
-def company():
-    return render_template('company.html')
-
-
-@app.route("/studentapplyjobs", methods=['GET', 'POST'])
-def studentapplyjobs():
-    return render_template('StudentApplyJobs.html')
-
-@app.route("/companylistadm", methods=['GET', 'POST'])
-def companylistadm():
-    return render_template('company_list_adm.html')
-
-@app.route("/assignsupervisor", methods=['GET', 'POST'])
-def assignsupervisor():
-    return render_template('assign-supervisor')
-
-@app.route("/studentreport", methods=['GET', 'POST'])
-def studentreport():
-    return render_template('Student_report.html')
-
-@app.route("/studentdetails", methods=['GET', 'POST'])
-def studentdetails():
-    return render_template('student-details.html')
-
-@app.route("/userpage", methods=['GET', 'POST'])
-def userpage():
-    return render_template('user_page.html')
-
-@app.route("/admin", methods=['GET', 'POST'])
-def admin():
-    return render_template('admin.html')
+# ... Define other routes and functions as needed
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
