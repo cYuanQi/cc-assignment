d35825a430e0447ea1854f23af445599e7fb971c
HewWK <143489299+HewWK@users.noreply.github.com>
2023-09-22 13:54:48 +0800

    Update Company.py

--- Company.py
+++ Company.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
-import pymysql 
+# Import additional modules
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
+from pymysql import connections
+import os
 import boto3
 from config import *
 
@@ -8,24 +10,18 @@ app = Flask(__name__)
 bucket = custombucket
 region = customregion
 
-# Configure your MySQL database connection
-db_conn = pymysql.connect(
+# MySQL database connection setup
+db_conn = connections.Connection(
     host=customhost,
     port=3306,
     user=customuser,
     password=custompass,
-    db=customdb,
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor
+    db=customdb
 )
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('company.html')
-
-@app.route("/company")
-def company():
     return render_template('company.html')
 
 @app.route("/postjob", methods=['POST'])
@@ -106,70 +102,119 @@ def postjob():
 def success():
     return "Job data submitted successfully!"
 
-@app.route("/nologin")
+    
+@app.route("/nologin", methods=['GET', 'POST'])
 def nologin():
     return render_template('no_login.html')
+
     
-@app.route("/about")
+@app.route("/about", methods=['GET', 'POST'])
 def about():
     return render_template('about.html')
 
-@app.route("/joblistings")
+@app.route("/joblistings", methods=['GET', 'POST'])
 def joblistings():
     return render_template('job-listings.html')
 
-@app.route("/jobsingle")
+@app.route("/jobsingle", methods=['GET', 'POST'])
 def jobsingle():
     return render_template('job-single.html')
 
-@app.route("/services")
+@app.route("/services", methods=['GET', 'POST'])
 def services():
     return render_template('services.html')
 
-@app.route("/servicesingle")
+@app.route("/servicesingle", methods=['GET', 'POST'])
 def servicesingle():
     return render_template('service-single.html')
 
-@app.route("/blog")
+@app.route("/blog", methods=['GET', 'POST'])
 def blog():
     return render_template('blog.html')
 
-@app.route("/blogsingle")
+@app.route("/blogsingle", methods=['GET', 'POST'])
 def blogsingle():
     return render_template('blog-single.html')
 
-@app.route("/portfolio")
+@app.route("/portfolio", methods=['GET', 'POST'])
 def portfolio():
     return render_template('portfolio.html')
 
-@app.route("/portfoliosingle")
+@app.route("/portfoliosingle", methods=['GET', 'POST'])
 def portfoliosingle():
     return render_template('portfolio-single.html')
 
-@app.route("/testimonials")
+@app.route("/testimonials", methods=['GET', 'POST'])
 def testimonials():
     return render_template('testimonials.html')
 
-@app.route("/faq")
+@app.route("/faq", methods=['GET', 'POST'])
 def faq():
     return render_template('faq.html')
 
-@app.route("/gallery")
+@app.route("/gallery", methods=['GET', 'POST'])
 def gallery():
     return render_template('gallery.html')
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
     return render_template('contact.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-@app.route("/studentApplication")
-def student_application():
-    return render_template('CompanyConfStudApp.html')
+@app.route("/lecture", methods=['GET', 'POST'])
+def lecture():
+    return render_template('lecture.html')
 
+@app.route("/lecturerdetails", methods=['GET', 'POST'])
+def lecturerdetails():
+    return render_template('lecturer-details.html')
+
+@app.route("/evaluatereport", methods=['GET', 'POST'])
+def evaluatereport():
+    return render_template('EvaluateReport.html')
+
+@app.route("/grade", methods=['GET', 'POST'])
+def grade():
+    return render_template('Grade.html')
+
+@app.route("/company", methods=['GET', 'POST'])
+def company():
+    return render_template('company.html')
+
+@app.route("/postjob", methods=['GET', 'POST'])
+def postjob():
+    return render_template('post-job.html')
+
+@app.route("/studentapplyjobs", methods=['GET', 'POST'])
+def studentapplyjobs():
+    return render_template('StudentApplyJobs.html')
+
+@app.route("/companylistadm", methods=['GET', 'POST'])
+def companylistadm():
+    return render_template('company_list_adm.html')
+
+@app.route("/assignsupervisor", methods=['GET', 'POST'])
+def assignsupervisor():
+    return render_template('assign-supervisor')
+
+@app.route("/studentreport", methods=['GET', 'POST'])
+def studentreport():
+    return render_template('Student_report.html')
+
+@app.route("/studentdetails", methods=['GET', 'POST'])
+def studentdetails():
+    return render_template('student-details.html')
+
+@app.route("/userpage", methods=['GET', 'POST'])
+def userpage():
+    return render_template('user_page.html')
+
+@app.route("/admin", methods=['GET', 'POST'])
+def admin():
+    return render_template('admin.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
