961cc500ca9fef147c5f7ebb279670f9343be7d7
HewWK <143489299+HewWK@users.noreply.github.com>
2023-09-23 16:50:52 +0800

    Update Company.py

--- Company.py
+++ Company.py
@@ -58,10 +58,12 @@ def postjob():
             cursor.execute(insert_sql, (email, job_title, job_location, job_region, job_type,  job_description, company_name, company_tagline, company_description,  company_website, facebook_username, twitter_username, linkedin_username))
           
             db_conn.commit()
-        
+            return render_template('success.html')
+         
     # If it's not a POST request, render the form
-    #return render_template('post-job.html')
-    return redirect(url_for('postjob1', message='Student have successfully approve'))
+    return render_template('post-job.html')
+
+
 
 @app.route("/approve_student", methods=["POST"])
 def approve_student():
